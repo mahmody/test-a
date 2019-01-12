323caaa0b7e14aa9b151baa40bb609a9b7893323
mahmody <42693632+mahmody@users.noreply.github.com>
2019-01-12 02:07:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,12 @@ const client = new Discord.Client();
 const fs = require('fs');
 const prefix = "+";
 
+  
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag} !`);
-          client.user.setActivity("you",{type: 'WATCHING'});
-  
-  });
-  
+    client.user.setStatus("dnd");
+ 
+ });
+
   client.on('message', message => {
     if(message.content == prefix + 'slist') {
              if(!message.author.id === '323885452207587329') return;
@@ -63,6 +63,37 @@ client.on('ready', () => {
     }
     });
     
+client.on('message', message => {
+    if (message.content.startsWith("$stats")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTitle('Stats Bot / Info ')
+            .addField('``Uptime``', timeCon(process.uptime()), true)
+            .addField('``Ping Is``' , `${Date.now() - message.createdTimestamp}` + '``Ms``', true)
+            .addField('``RAM Usage``', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
+            .addField('``Guild Count``', client.guilds.size, true)
+            .addField('``Bot In channel``' , `${client.channels.size}` , true)
+            .addField('``Users rout``' ,`${client.users.size}` , true)
+            .addField('``Name Bot Or tag``' , `${client.user.tag}` , true)
+            .addField('``Bot Id``' , `${client.user.id}` , true)
+            .setFooter('SmiLeBoT / Team')
+    })
+}
+});
+
+
+function timeCon(time) {
+    let days = Math.floor(time % 31536000 / 86400)
+    let hours = Math.floor(time % 31536000 % 86400 / 3600)
+    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
+    days = days > 9 ? days : '0' + days
+    hours = hours > 9 ? hours : '0' + hours
+    minutes = minutes > 9 ? minutes : '0' + minutes
+    seconds = seconds > 9 ? seconds : '0' + seconds
+    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
+}
     
     
     client.login(process.env.BOT_TOKEN);
